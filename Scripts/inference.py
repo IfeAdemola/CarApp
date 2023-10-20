78f7d519c4a268614b8b4e4db44f1e78ed0180e6
IfeAdemola <ifeoluwaademola17@gmail.com>
2023-10-20 12:46:54 +0200

    Changes to inference.py

--- Scripts/inference.py
+++ Scripts/inference.py
@@ -3,5 +3,5 @@ import numpy as np
 import pickle
 
 def predict(data):
-    model = pickle.load(open('model.pkl','rb'))
+    model = pickle.load(open(r'C:\Users\ifeol\projects\CarApp\model.pkl','rb'))
     return model.predict(data)
