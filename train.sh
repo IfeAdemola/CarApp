dc7f093450c0fca9875f25240bbf859e7f3c9a39
IfeAdemola <ifeoluwaademola17@gmail.com>
2023-10-21 19:27:15 +0200

    Changes to files

---
+++ train.sh
@@ -0,0 +1,4 @@
+python train.py --data_path datasets/raw-data/CarPrice_Assignment.csv \
+        	    --model_path artifacts/models/model.joblib \
+                --ct_path artifacts/models/ct.joblib 
+
