4fd7ec2a3d915abb650b293ee1cab04f004e400f
IfeAdemola <ifeoluwaademola17@gmail.com>
2023-10-31 09:35:45 +0100

    Changes to app.py

--- app.py
+++ app.py
@@ -1,38 +1,45 @@
 import streamlit as st
 import numpy as np
-from inference import predict
+from preprocessing import inference_preprocessor
+from inference import load_model, predict
 
-st.title('Car App') 
-st.markdown('Predicting the price of a car based on the attributes of the car')
+model_path = 'artifacts/models/model.joblib'
+preprocessor_path = 'artifacts/models/ct.joblib'
 
-st.sidebar.markdown("## Variable Selector")
-volume = st.sidebar.number_input('Volume of car')
-curbweight = st.sidebar.number_input('Curbweight of car')
-peakrpm = st.sidebar.number_input('Peakrpm of car')
-citympg = st.sidebar.number_input('Citympg of car')
-horsepower = st.sidebar.number_input('Horsepower of car')
+@st.cache
+def load_models(model_path, preprocessor_path):  
+    model = load_model(model_path)
+    preprocessor = load_model(preprocessor_path)
+    return model, preprocessor
 
-fueltype = st.sidebar.selectbox(
-"What type of fuel does the car use?",
-("Diesel", "Gas"),
-index=None,
-placeholder="Select fuel type...",
-)
+def main():    
+    st.title('Car App') 
+    st.markdown('Predicting the price of a car based on the attributes of the car')
+    st.sidebar.markdown("## Variable Selector")
 
-if fueltype == "Diesel":
-    fueltype_diesel = 1
-    fueltype_gas = 0
-else:
-    fueltype_diesel = 0
-    fueltype_gas = 1
+    horsepower = st.sidebar.number_input('Horsepower of car')
+    fueltype = st.sidebar.selectbox(
+    "What type of fuel does the car use?",
+    ("diesel", "gas"),
+    index=None,
+    placeholder="Select fuel type...",
+    )
 
-if st.button('Predict car price'):
-    data = np.array([[volume, curbweight, peakrpm, citympg, horsepower, fueltype_diesel, fueltype_gas]])
-    price = predict(data)
-    st.write(price[0])
-          
+    model, preprocessor = load_models(model_path, preprocessor_path)
 
+    if st.button('Predict car price'):
+        price = run(model, preprocessor,horsepower, fueltype)
+        st.markdown(f"**The price of your car is ${price:.2f}**")
 
 
+def run(model, preprocessor, horsepower, fueltype):
+    
+    data = inference_preprocessor(fueltype, float(horsepower), preprocessor)
+    price = predict(model, data)
+    return price
+
+
+if __name__ == "__main__":
+    main()
 
 
