72e8e041232051cbfe6c5f0cadf02e625d15506b
IfeAdemola <ifeoluwaademola17@gmail.com>
2023-12-09 09:49:37 +0100

    Deleted predict.sh, app/app.py and app/requirements.py

--- predict.sh
+++
@@ -1,6 +0,0 @@
-python inference.py --model_path artifacts/models/model.joblib \
-                    --ct_path artifacts/models/ct.joblib \
-                    --fueltype gas \
-                    --horsepower 4500
-
-
